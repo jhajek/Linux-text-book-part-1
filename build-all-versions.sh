13a2f39922b048c584aba7d0e37c889035a91991
Jeremy Hajek <hajek@iit.edu>
2015-06-12 13:39:59 -0500

    fixed pathing issues ../ to ./

--- build-all-versions.sh
+++ build-all-versions.sh
@@ -10,13 +10,13 @@ rm -rf ./output/*
 #[Getting-Started](http://pandoc.org/getting-started.html)
 
 #If you want to create a PDF, you’ll need to have LaTeX installed. (See MacTeX on OS X, MiKTeX on Windows, or install the texlive package in linux.) Then do
-pandoc ../Linux-Technology-and-Philosophy-Part-I.md -s -o ../output/Linux-Technology-and-Philosophy-Part-I.pdf
+pandoc ./Linux-Technology-and-Philosophy-Part-I.md -s -o ./output/Linux-Technology-and-Philosophy-Part-I.pdf
 
 #To convert it to HTML, use this command:
-pandoc ../Linux-Technology-and-Philosophy-Part-I.md -f markdown -t html -s -o ../output/Linux-Technology-and-Philosophy-Part-I.html
+pandoc ./Linux-Technology-and-Philosophy-Part-I.md -f markdown -t html -s -o ./output/Linux-Technology-and-Philosophy-Part-I.html
 
 #[Convert your Markdown file to Word (docx):](http://bob.yexley.net/generate-a-word-document-from-markdown-on-os-x/)
-pandoc -o ../output/Linux-Technology-and-Philosophy-Part-I.docx -f markdown -t docx ../Linux-Technology-and-Philosophy-Part-I.md
+pandoc -o ./output/Linux-Technology-and-Philosophy-Part-I.docx -f markdown -t docx ./Linux-Technology-and-Philosophy-Part-I.md
 
 #[Convert to epub](http://pandoc.org/epub.html)
-pandoc ../Linux-Technology-and-Philosophy-Part-I.md -o ../output/Linux-Technology-and-Philosophy-Part-I.epub
+pandoc ./Linux-Technology-and-Philosophy-Part-I.md -o ./output/Linux-Technology-and-Philosophy-Part-I.epub
