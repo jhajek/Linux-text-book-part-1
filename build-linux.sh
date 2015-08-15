a069d1ce4f3e4176196552d5a3108af03176d357
Jeremy Hajek <hajek@iit.edu>
2015-08-15 17:01:19 -0500

    modified build linux script to actually build on linux

--- build-linux.sh
+++ build-linux.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
-set -x
-set -e
-
-sudo apt-get -y install pandoc pandoc-citeproc texlive
-
-#delete previous build artifacts but not the README.md
-rm -rf ./output/*.epub ./output/*.html ./output/*.pdf ./output/*.docx
-
 #[Getting-Started](http://pandoc.org/getting-started.html)
 
-#If you want to create a PDF, you’ll need to have LaTeX installed. (See MacTeX on OS X, MiKTeX on Windows, or install the texlive package in linux.) Then do
-pandoc ./Linux-Technology-and-Philosophy-Part-II.md -s -o ./output/Linux-Technology-and-Philosophy-Part-II.pdf
+#delete previous build artifacts but not the README.md
+rm -f ./output/pdf/*.pdf
+rm -f ./output/epub/*.epub
+rm -f ./output/html/*.html
+rm -f ./output/docx/*.docx
 
-#To convert it to HTML, use this command:
-pandoc ./Linux-Technology-and-Philosophy-Part-II.md -f markdown -t html -s -o ./output/Linux-Technology-and-Philosophy-Part-II.html
+#######################################################################################################################
+# PDF - #If you want to create a PDF, you’ll need to have LaTeX installed. (See MacTeX on OS X, MiKTeX on Windows, or
+# install the texlive package in linux.) Then do                
+########################################################################################################################
+# --chapters starts chapters on odd number pages
+pandoc --toc -V geometry:margin=1in --number-sections -s -o ./output/pdf/Understanding-the-Technology-and-Philosophy-of-Linux-Part-I.pdf ./title/title.txt ./Chapter-01/chapter-01.md ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md ./Chapter-04/chapter-04.md ./Chapter-05/chapter-05.md ./Chapter-06/chapter-06.md ./Chapter-07/chapter-07.md ./Chapter-08/chapter-08.md ./Chapter-09/chapter-09.md ./Chapter-10/chapter-10.md ./Chapter-11/chapter-11.md ./Chapter-12/chapter-12.md ./Chapter-13/chapter-13.md ./Chapter-14/chapter-14.md ./Chapter-15/chapter-15.md ./Chapter-16/chapter-16.md ./Glossary-A/Glossary-A.md ./Glossary-B/Glossary-B.md 
+# http://stackoverflow.com/questions/19397100/adding-a-title-page-page-headers-and-footers-using-pandoc
 
-#[Convert your Markdown file to Word (docx):](http://bob.yexley.net/generate-a-word-document-from-markdown-on-os-x/)
-pandoc -o ./output/Linux-Technology-and-Philosophy-Part-II.docx -f markdown -t docx ./Linux-Technology-and-Philosophy-Part-II.md
+#################################################################
+# EPUB - To convert it to EPUB, use this command:               #
+#################################################################
+
+pandoc --toc --number-sections -S -o ./output/epub/Understanding-the-Technology-and-Philosophy-of-Linux-Part-I.epub ./title/title.txt  ./Chapter-01/chapter-01.md ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md ./Chapter-04/chapter-04.md ./Chapter-05/chapter-05.md ./Chapter-06/chapter-06.md ./Chapter-07/chapter-07.md ./Chapter-08/chapter-08.md ./Chapter-09/chapter-09.md ./Chapter-10/chapter-10.md ./Chapter-11/chapter-11.md ./Chapter-12/chapter-12.md ./Chapter-13/chapter-13.md ./Chapter-14/chapter-14.md ./Chapter-15/chapter-15.md ./Chapter-16/chapter-16.md ./Glossary-A/Glossary-A.md ./Glossary-B/Glossary-B.md 
+ 
+##################################################################
+# HTML5 - To convert it to HTML, use this command:              #
+################################################################## 
+
+pandoc --toc -S -f markdown -t html5 -o ./output/html/Understanding-the-Technology-and-Philosophy-of-Linux-Part-I.html ./title/title.txt ./Chapter-01/chapter-01.md ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md ./Chapter-04/chapter-04.md ./Chapter-05/chapter-05.md ./Chapter-06/chapter-06.md ./Chapter-07/chapter-07.md ./Chapter-08/chapter-08.md ./Chapter-09/chapter-09.md ./Chapter-10/chapter-10.md ./Chapter-11/chapter-11.md ./Chapter-12/chapter-12.md ./Chapter-13/chapter-13.md ./Chapter-14/chapter-14.md  ./Chapter-15/chapter-15.md ./Chapter-16/chapter-16.md ./Glossary-A/Glossary-A.md ./Glossary-B/Glossary-B.md 
+
+#############################################################################################################################
+# DOCX - [Convert your Markdown file to Word (docx):](http://bob.yexley.net/generate-a-word-document-from-markdown-on-os-x/)#############################################################################################################################
+
+pandoc --toc -o ./output/docx/Understanding-the-Technology-and-Philosophy-of-Linux-Part-I.docx -f markdown -t docx ./title/title.txt  ./Chapter-01/chapter-01.md ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md ./Chapter-04/chapter-04.md ./Chapter-05/chapter-05.md ./Chapter-06/chapter-06.md ./Chapter-07/chapter-07.md ./Chapter-08/chapter-08.md ./Chapter-09/chapter-09.md ./Chapter-10/chapter-10.md ./Chapter-11/chapter-11.md ./Chapter-12/chapter-12.md ./Chapter-13/chapter-13.md ./Chapter-14/chapter-14.md ./Chapter-15/chapter-15.md ./Chapter-16/chapter-16.md ./Glossary-A/Glossary-A.md ./Glossary-B/Glossary-B.md 
+
+######################
+# Debugging          #
+######################
+
+#cp  C:\Users\palad\Documents\Linux-text-book-part-1\output\pdf\*.pdf  C:\Users\palad\OneDrive\Documents\epub
+#cp  C:\Users\palad\Documents\Linux-text-book-part-1\output\epub\*.epub  C:\Users\palad\OneDrive\Documents\epub
 
 #[Convert to epub](http://pandoc.org/epub.html)
-pandoc ./Linux-Technology-and-Philosophy-Part-II.md -o ./output/Linux-Technology-and-Philosophy-Part-II.epub
+#pandoc ./Chapter-Table-of-Contents/chapter-toc.md ./Introduction/introduction.md ./Chapter-01/chapter-01.md -o ./output/Linux-Technology-and-Philosophy-Part-II.epub
+
+# ODT demo http://pandoc.org/demos.html
+#pandoc ./Chapter-Table-of-Contents/chapter-toc.md ./Introduction/introduction.md ./Chapter-01/chapter-01.md -o ./output/Linux-Technology-and-Philosophy-Part-II.odt
