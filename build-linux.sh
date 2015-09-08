8d53ac39a01a4ca2b973150855ecd95a676d3a25
Jeremy Hajek <hajek@iit.edu>
2015-09-08 01:29:06 -0500

    Added the correct PDF build parameters on the build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,7 @@ set -e
 
 #[Getting-Started](http://pandoc.org/getting-started.html)
 
-STAMP=`date +%m%d%y-%k%M%S`
+STAMP=`date +%m%d%y-%K%M%S`
 
 
 #delete previous build artifacts but not the README.md
@@ -18,7 +18,7 @@ rm -f ./output/docx/*.docx
 # install the texlive package in linux.) Then do                
 ########################################################################################################################
 # --chapters starts chapters on odd number pages
-pandoc --toc -V geometry:margin=1in --number-sections -s -o ./output/pdf/Understanding-the-Technology-and-Philosophy-of-Linux-Part-I-$STAMP.pdf ./title/title.txt ./Chapter-01/chapter-01.md ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md ./Chapter-04/chapter-04.md ./Chapter-05/chapter-05.md ./Chapter-06/chapter-06.md ./Chapter-07/chapter-07.md ./Chapter-08/chapter-08.md ./Chapter-09/chapter-09.md ./Chapter-10/chapter-10.md ./Chapter-11/chapter-11.md ./Chapter-12/chapter-12.md ./Chapter-13/chapter-13.md ./Chapter-14/chapter-14.md ./Chapter-15/chapter-15.md ./Glossary-A/Glossary-A.md ./Glossary-B/Glossary-B.md 
+pandoc --toc -V geometry:margin=1in --number-sections --include-in-header ./title/titlesec.tex -s -o ./output/pdf/Understanding-the-Technology-and-Philosophy-of-Linux-Part-I-$STAMP.pdf ./title/title.txt ./Chapter-01/chapter-01.md ./Chapter-02/chapter-02.md ./Chapter-03/chapter-03.md ./Chapter-04/chapter-04.md ./Chapter-05/chapter-05.md ./Chapter-06/chapter-06.md ./Chapter-07/chapter-07.md ./Chapter-08/chapter-08.md ./Chapter-09/chapter-09.md ./Chapter-10/chapter-10.md ./Chapter-11/chapter-11.md ./Chapter-12/chapter-12.md ./Chapter-13/chapter-13.md ./Chapter-14/chapter-14.md ./Chapter-15/chapter-15.md ./Glossary-A/Glossary-A.md ./Glossary-B/Glossary-B.md 
 # http://stackoverflow.com/questions/19397100/adding-a-title-page-page-headers-and-footers-using-pandoc
 
 #################################################################
