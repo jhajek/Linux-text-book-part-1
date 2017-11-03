ca4eff9957e7ebb2ea9e04803182acce47d23ce8
Jeremy Hajek <hajek@iit.edu>
2017-11-02 23:54:33 -0500

    Updated install script

--- files/Chapter-08/install-java-8.sh
+++ files/Chapter-08/install-java-8.sh
@@ -23,7 +23,7 @@ sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true
 sudo apt-get -y update; 
 sudo apt-get install -y oracle-java8-installer
 
-sudo apt-get -y update ; sudo apt-get -y install pkgconf wget liblzo2-dev sysstat iotop vim libssl-dev libsnappy-dev libsnappy-java libbz2-dev libgcrypt11-dev zlib1g-dev lzop htop fail2ban
+sudo apt-get -y update ; sudo apt-get -y install pkgconf wget liblzo2-dev sysstat iotop vim libssl-dev libsnappy-dev libsnappy-java libbz2-dev libgcrypt11-dev zlib1g-dev lzop htop
 
 # Download Hadoop 2.6.5 source and extract tarbal
 cd /tmp; wget http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz
@@ -32,23 +32,9 @@ tar -xvzf hadoop-2.6.5.tar.gz
 # Move hadoop directory to ZFS stripe
 mv /tmp/hadoop-2.6.5 /datapool1
 
-# Pre-seed rsa keys for connection
-mkdir ~/.ssh
-chmod 600 ~/hajek/itmd-521/hadoop-conf/production-cluster/id_rsa 
-cp -v ~/hajek/itmd-521/hadoop-conf/production-cluster/id_rsa ~/.ssh; 
-cp -v ~/hajek/itmd-521/hadoop-conf/production-cluster/config ~/.ssh 
-cat ~/hajek/itmd-521/hadoop-conf/production-cluster/id_rsa.pub >> ~/.ssh/authorized_keys
 
-##
-# Add Native compiled Bzip compression .so
-mv -v /datapool1/hadoop-2.6.5/lib/native /datapool1/hadoop-2.6.5/lib/native-old
-cp -v ~/hajek/itmd-521/hadoop-conf/compiled-native/native.tar.gz /datapool1/hadoop-2.6.5/lib
 
-cd /datapool1/hadoop-2.6.5/lib/
-tar -xvf native.tar.gz
-
-echo "Extracted new native libraries\n"
-
+# make sure you have installed the fail2ban application via apt-get
 # use sed to replace lines in the /etc/fail2ban/jail.conf files
 sudo sed -i "s/ignoreip = 127.0.0.1\/8/ignoreip = 127.0.0.01\/8 64.131.111.178/g" /etc/fail2ban/jail.conf
 #http://www.fail2ban.org/wiki/index.php/MANUAL_0_8#Jails
