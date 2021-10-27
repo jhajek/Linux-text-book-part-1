103fec8709cf56b57d5056fcf91c44e6f41de1d8
Jeremy R Hajek <hajek@iit.edu>
2021-10-26 21:31:51 -0500

    update: correct dpkg install command

--- install-linux-dependencies.sh
+++ install-linux-dependencies.sh
@@ -2,10 +2,17 @@
 
 sudo apt-get -y update
 sudo apt-get install wget git
-wget https://github.com/jgm/pandoc/releases/download/2.15/pandoc-2.15-1-amd64.deb
-sudo dpkg -i pandoc-2.2.2.1-1-amd64.deb
-sudo apt-get -y install texlive texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-xetex texlive-math-extra
+if [ -e ./pandoc-2.15-1-amd64.deb ]
+  then
+    sudo dpkg -i pandoc-2.15-1-amd64.deb
+    rm ./pandoc-2.15-1-amd64.deb
+  else
+    wget https://github.com/jgm/pandoc/releases/download/2.15/pandoc-2.15-1-amd64.deb
+    sudo dpkg -i pandoc-2.15-1-amd64.deb
+    rm ./pandoc-2.15-1-amd64.deb
+fi
 
+sudo apt-get -y install texlive texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-xetex texlive-math-extra
 
 wget http://packages.sil.org/sil.gpg
 sudo apt-key add sil.gpg
