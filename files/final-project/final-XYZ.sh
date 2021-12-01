f99e77a2467d0c80f83dfca8d4c9ea5d362a8cf9
Jeremy R Hajek <hajek@iit.edu>
2021-12-01 10:19:49 -0600

    update: added clarification to the final-project sample template

--- files/final-project/final-XYZ.sh
+++ files/final-project/final-XYZ.sh
@@ -3,19 +3,53 @@
 ###################################################
 # Final Project Submission by: FULLNAME GOES HERE #
 ###################################################
+# Use the sleep command to make the script sleep so you can read the output
+# The echo commands are for me to break up the output and a guide for you to see 
+# the results.  
+# Place your code inbetween the echo and the sleep command, per section
 
 #### Section One
+echo "Starting Section 1"
 
+
+
+
+sleep 2
 #### Section Two
+echo "Starting Section 2"
 
+
+
+
+sleep 2
 #### Section Three
+echo "Starting Section 3"
 
+
+
+sleep 2
 #### Section Four
+echo "Starting Section 4"
 
+
+sleep 2
 #### Section Five
+echo "Starting Section 5"
 
+
+sleep 2
 #### Section Six
+echo "Starting Section 6"
 
+
+sleep 2
 #### Section Seven
+echo "Starting Section 7"
 
+
+sleep 2
 #### Section Eight
+echo "Starting Section 8"
+
+
+sleep 2
