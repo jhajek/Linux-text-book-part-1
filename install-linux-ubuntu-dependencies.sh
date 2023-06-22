e5c6f69d31895986cd1a03819ca7608503c19d48
Jeremy Hajek <hajek@iit.edu>
2023-06-22 14:04:36 -0500

    update pandoc to 3.1.3

--- install-linux-ubuntu-dependencies.sh
+++ install-linux-ubuntu-dependencies.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get -y update
 sudo apt-get install wget git
-if [ -e ./pandoc-2.15-1-amd64.deb ]
+if [ -e ./pandoc-3.1.3-1-amd64.deb ]
   then
-    sudo dpkg -i pandoc-2.15-1-amd64.deb
-    rm ./pandoc-2.15-1-amd64.deb
+    sudo dpkg -i pandoc-3.1.3-1-amd64.deb
+    rm ./pandoc-3.1.3-1-amd64.deb
   else
-    wget https://github.com/jgm/pandoc/releases/download/2.15/pandoc-2.15-1-amd64.deb
-    sudo dpkg -i pandoc-2.15-1-amd64.deb
-    rm ./pandoc-2.15-1-amd64.deb
+    wget https://github.com/jgm/pandoc/releases/download/3.1.3/pandoc-3.1.3-1-amd64.deb
+    sudo dpkg -i pandoc-3.1.3-1-amd64.deb
+    rm ./pandoc-3.1.3-1-amd64.deb
 fi
 
 sudo apt-get install -y texlive texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-xetex texlive-font-utils librsvg2-bin texlive-science-doc texlive-science
