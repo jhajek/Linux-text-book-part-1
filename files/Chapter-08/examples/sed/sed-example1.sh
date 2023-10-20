efb1b6839beed65bea018f465bf237d21428deef
Jeremy Hajek <hajek@iit.edu>
2023-10-20 14:29:45 -0500

    update sed example

--- files/Chapter-08/examples/sed/sed-example1.sh
+++ files/Chapter-08/examples/sed/sed-example1.sh
@@ -2,4 +2,4 @@ sed -e '/^#/d' hosts.deny | head
 
 sudo sed -i "s/bantime = 600/bantime = 10000/g" /etc/fail2ban/jail.conf
 
-sudo sed -i "s/joseph/Joseph/g" names.txt
+sed -i "s/joseph/Joseph/g" names.txt
