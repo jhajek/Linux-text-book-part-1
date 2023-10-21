d884f8cb85eb6e8b226b93e892a873911325d337
Jeremy Hajek <hajek@iit.edu>
2023-10-20 23:10:11 -0500

    update samples

--- files/Chapter-08/examples/sed/sed-example1.sh
+++ files/Chapter-08/examples/sed/sed-example1.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/bash
+
+# This example is similar to the AWK example to display all lines that do not have a # at the start
 sed -e '/^#/d' hosts.deny | head
 
-sudo sed -i "s/bantime = 600/bantime = 10000/g" /etc/fail2ban/jail.conf
+# This replaces the bantime =1h to bantime =10h 
+sudo sed -i "s/bantime      = 1h/bantime      = 10h/g" /etc/fail2ban/jail.conf
 
+# This will capitalize the name joseph to Joseph
 sed -i "s/joseph/Joseph/g" names.txt
