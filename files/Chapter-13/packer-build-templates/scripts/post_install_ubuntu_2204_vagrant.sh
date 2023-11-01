4f5876b36783bd55f6a76fe4d2a8e950bd5d73ac
Jeremy Hajek <hajek@iit.edu>
2023-10-31 21:46:09 -0500

    update spelling

--- files/Chapter-13/packer-build-templates/scripts/post_install_ubuntu_2204_vagrant.sh
+++ files/Chapter-13/packer-build-templates/scripts/post_install_ubuntu_2204_vagrant.sh
@@ -6,5 +6,5 @@ set -v
 # Add User customizations below here
 ##################################################
 sudo apt update
-sudo apt install -y links rysnc
+sudo apt install -y links rsync
 echo "Created by me!" >> /home/vagrant/success.txt
