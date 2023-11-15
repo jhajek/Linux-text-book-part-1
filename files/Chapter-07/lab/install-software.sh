508685d69ac8cedc5efc47bb0490aff944c39c2c
Jeremy Hajek <hajek@iit.edu>
2023-11-15 13:31:23 -0600

    updated additional example for find and replace

--- files/Chapter-07/lab/install-software.sh
+++ files/Chapter-07/lab/install-software.sh
@@ -4,5 +4,7 @@ set -v
 
 # Dependencies needed to install the graphite-api 
 # We need to manually retrieve all the dependencies
-sudo apt-get install python-cairocffi python-flask python-pyparsing python-tz python-tzlocal libjs-sphinxdoc sphinx-rtd-theme-common
+sudo apt install python-pip python-setuptools python-dev
+
+sudo apt install python-cairocffi python-flask python-pyparsing python-tz python-tzlocal libjs-sphinxdoc sphinx-rtd-theme-common
 
