ffc0ab5333b4e597191997f57b7971ccc324f813
Jeremy <hajek@iit.edu>
2024-10-23 13:17:53 -0500

    update adding code to disable ssh password auth

--- files/Chapter-13/packer-build-templates/scripts/post_install_ubuntu_2205_vagrant.sh
+++ files/Chapter-13/packer-build-templates/scripts/post_install_ubuntu_2205_vagrant.sh
@@ -8,3 +8,6 @@ set -v
 sudo apt update
 sudo apt install -y links rsync
 echo "Created by me!" >> /home/vagrant/success.txt
+
+# Customization to disable password auth over ssh
+echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config.d/diable-password-auth.conf
