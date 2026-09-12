f246a965040c515b37f50b8a3877f77d33dff352
Jeremy <hajek@iit.edu>
2026-09-11 21:59:47 -0500

    update add command to push-release directly from cloned repo -- only work if you have already authenticated via gh cli

---
+++ push-release.ps1
@@ -0,0 +1,15 @@
+# Code for pushing a textbook release to GitHub via the gh cli
+# This requires your device or laptop to be pre-authenticated with GitHub
+# otherwise this won't work
+
+$STAMP=Get-Date(Get-Date).ToUniversalTime() -uformat "%Y-%m-%d.%H%M%S"
+$PDF_PATH='./output/pdf/Understanding-Free-and-Opensource-Operating-Systems-Part-I.pdf'
+$EPUB_PATH='./output/epub/Understanding-Free-and-Opensource-Operating-Systems-Part-I.epub'
+
+git tag -a $STAMP -m "Opensource Textbook build at $STAMP"
+git push origin $STAMP
+
+# Retrieve PREVIOUS commit tag
+$PREV=git describe --tags --abbrev=0 HEAD^
+
+gh release create $STAMP $PDF_PATH $EPUB_PATH --title $STAMP --notes $(git log $PREV --pretty=format:'- %s (%h)')
